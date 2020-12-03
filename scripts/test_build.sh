d039880c6410dd4a7c8ed84f0944b83a14ea5bb6
PRADEEPKJ <PRADEEPKIRUVALE@gmail.com>
2020-12-03 18:54:15 +0530

    enable all

--- scripts/test_build.sh
+++ scripts/test_build.sh
@@ -1,21 +1,22 @@
 #!/usr/bin/bash
 
 #check the format of the rust code
-:'
-result=$(cargo fmt -- --check)
-echo "format $result"
-
+if ! cargo fmt -- --check;
+then
+   exit 1
+fi
 # Lint Checking
-result=$(cargo clippy)
-echo "clippy $result"
-'
+if ! cargo clippy;
+then
+   exit 1
+fi
 # Run tests
-#result=$(cargo test  --verbose)
 if ! cargo test --verbose;
 then
-  #echo "test passed"
   exit 1
 fi
 # Build
-result=$(cargo build --release)
-echo $result
+if ! cargo build --release;
+then
+  exit 1
+fi  
